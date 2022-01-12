b8a4361c07f6ef5e0867b97ae099c50ce5a4606c
paulandre <paulandre@pebvaio>
2022-01-12 14:09:02 -0600

    Update 10

--- src/main/java/com/example/cinetrailer/controller/HomeController.java
+++ src/main/java/com/example/cinetrailer/controller/HomeController.java
@@ -12,11 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class HomeController {
 
